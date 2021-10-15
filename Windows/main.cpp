0cadef6fff0d35635baaba1f08a9eab575bd3b4a
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-10-15 13:34:01 +0200

    Updated main.cpp

--- Windows/main.cpp
+++ Windows/main.cpp
@@ -3,13 +3,27 @@
 
 namespace MAIN {
     
+    int init() {
+        
+        
+        
+    };
+    
     int DAPP() {
         
         string dapps = {};
         
         int runDapp(int reward, string dappId, string datas) {
             
-            
+            if (dappId != dapps) {
+                
+                
+                
+            } else {
+                
+                GUI.logs.push_back("The Marscoin Digital App ID is not in the DAPPs list");
+                
+            };
             
         };
         int stopDapp(string dappId) {
@@ -22,11 +36,12 @@ namespace MAIN {
     int GUI() {
         
         int app = { "appXSize": 1920, "appYSize": 1080, "buttons": { "home": { "xSize": 100, "ySize": 50, "images": { "dark": "", "light": "" } }, "transactions": { "xSize": 100, "ySize": 50, "images": { "dark": "", "light": "" } }, "options": { "xSize": 100, "ySize": 50, "images": { "dark": "", "light": "" } } } };
+        int logs = [];
         
     };
     int INTERNET() {
         
-        
+        string peers = [];
         
     };
     int THREADING() {
@@ -57,15 +72,15 @@ namespace MAIN {
         };
         int createPublicKey(int type, string privatekey) {
             
-            if (type == 0) {
+            if (type == 0) { // if the public key type is 
                 
                 
                 
-            } else if (type == 1) {
+            } else if (type == 1) { // if the public key type is 
                 
                 
                 
-            } else if (type == 2) {
+            } else if (type == 2) { // if the public key type is
                 
                 
                 
