a4adde9254c369ec9b54a68672191f4bbc250ce6
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-10-15 12:48:44 +0200

    Created main.cpp

---
+++ Windows/main.cpp
@@ -0,0 +1,62 @@
+#include <list>
+#include <string>
+
+namespace MAIN {
+    
+    int DAPP() {
+        
+        string dapps = {};
+        
+        int runDapp(int reward, string dappId, string datas) {
+            
+            
+            
+        };
+        int stopDapp(string dappId) {
+            
+            
+            
+        };
+        
+    };
+    int GUI() {
+        
+        string images = {};
+        
+    };
+    int INTERNET() {
+        
+        
+        
+    };
+    int THREADING() {
+        
+        
+        
+    };
+    int WALLET() {
+        
+        int createWallet(string name, string ext, string privatekeys, string publickeys) {
+            
+            
+            
+        };
+        int readWallet(string name, string password) {
+            
+            
+            
+        };
+        int createPrivateKey(int type) {
+            
+            
+            
+        };
+        int createPublicKey(int type, string privatekey) {
+            
+            
+            
+        };
+        
+    };
+    
+};
