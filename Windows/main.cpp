dd3e403682df83881f22cf50d3de785b853fecce
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-01-30 10:28:40 +0100

    Updated main.cpp

--- Windows/main.cpp
+++ Windows/main.cpp
@@ -1,92 +1,23 @@
 #include <list>
 #include <string>
 
+#include "blockchain/blockchain.cpp"
+#include "config/config.cpp"
+#include "instructions/instructions.cpp"
+#include "internet/internet.cpp"
+#include "nodefs/nodefs.cpp"
+#include "ui/ui.cpp"
+#include "wallet/wallet.cpp"
+
 namespace MAIN {
     
     int init() {
         
-        
-        
-    };
-    
-    int DAPP() {
-        
-        string dapps = {};
-        
-        int runDapp(int reward, string dappId, string datas) {
-            
-            if (dappId != dapps) {
-                
-                
-                
-            } else {
-                
-                GUI.logs.push_back("The Marscoin Digital App ID is not in the DAPPs list");
-                
-            };
-            
-        };
-        int stopDapp(string dappId) {
-            
-            
-            
-        };
-        
-    };
-    int GUI() {
-        
-        int app = { "appXSize": 1920, "appYSize": 1080, "buttons": { "home": { "xSize": 100, "ySize": 50, "images": { "dark": "", "light": "" } }, "transactions": { "xSize": 100, "ySize": 50, "images": { "dark": "", "light": "" } }, "options": { "xSize": 100, "ySize": 50, "images": { "dark": "", "light": "" } } } };
-        int logs = [];
-        
-    };
-    int INTERNET() {
-        
-        string peers = [];
-        
-    };
-    int THREADING() {
-        
-        
-        
-    };
-    int WALLET() {
-        
-        int createWallet(string name, string ext, string privatekeys, string publickeys) {
-            
-            
-            
-        };
-        int readWallet(string name, string password) {
-            
-            
-            
-        };
-        int createPrivateKey(int type) {
-            
-            if (type == 0) {
-                
-                
-                
-            };
-            
-        };
-        int createPublicKey(int type, string privatekey) {
-            
-            if (type == 0) { // if the public key type is 
-                
-                
-                
-            } else if (type == 1) { // if the public key type is 
-                
-                
-                
-            } else if (type == 2) { // if the public key type is
-                
-                
-                
-            };
-            
-        };
+        _UI_.init();
+        _NODEFileSystem_.init();
+        _WALLET_.init(config["datas_path"["wallet_file"]]);
+        _INTERNET_.init();
+        _BLOCKCHAIN_.init();
         
     };
     
