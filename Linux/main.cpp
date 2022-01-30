f5cb429e936a3308e1ade6d1adbf33ae48957938
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-01-30 09:53:37 +0100

    Created main.cpp

---
+++ Linux/main.cpp
@@ -0,0 +1,17 @@
+#include "/blockchain/blockchain.cpp"
+#include "/instructions/instructions.cpp"
+#include "/internet/internet.cpp"
+#include "/nodefs/nodefs.cpp"
+#include "/ui/ui.cpp"
+#include "/wallet/wallet.cpp"
+
+
+namespace _MAIN_ {
+    
+    int _INIT_() {
+        
+        
+        
+    };
+    
+}
