8c4a920836378ef6edc0ee6507dac039d2bfaab5
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-01-30 10:23:11 +0100

    Updated main.cpp

--- MacOS/main.cpp
+++ MacOS/main.cpp
@@ -1,4 +1,5 @@
 #include "/blockchain/blockchain.cpp"
+#include "/configs/default.json"
 #include "/instructions/instructions.cpp"
 #include "/internet/internet.cpp"
 #include "/nodefs/nodefs.cpp"
@@ -12,7 +13,7 @@ namespace _MAIN_ {
         
         _UI_.init();
         _NODEFileSystem_.init();
-        _WALLET_.init();
+        _WALLET_.init(config["datas_path"["wallet_file"]]);
         _INTERNET_.init();
         _BLOCKCHAIN_.init();
         
