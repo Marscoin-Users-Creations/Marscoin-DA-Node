afa9a4d38760c2eb4960242de2a501b8e2bdd731
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-02-01 07:37:23 +0100

    Updated main.cpp

--- Linux/main.cpp
+++ Linux/main.cpp
@@ -18,6 +18,27 @@ namespace _MAIN_ {
         _BLOCKCHAIN_.init();
         _INSTRUCTIONS_.init();
         
+        if (peers.size() >= 32) {
+            
+            _INTERNET_.client0.connect(peers[0]);
+            _INTERNET_.client1.connect(peers[1]);
+            _INTERNET_.client2.connect(peers[2]);
+            _INTERNET_.client3.connect(peers[3]);
+            _INTERNET_.client4.connect(peers[4]);
+            _INTERNET_.client5.connect(peers[5]);
+            _INTERNET_.client6.connect(peers[6]);
+            _INTERNET_.client7.connect(peers[7]);
+            
+        } else {
+            
+            for (peers.size()) {
+                
+                _INTERNET_.client
+                
+            };
+            
+        };
+        
     };
     
 }
